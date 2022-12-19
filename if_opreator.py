7d7660a066618bb29cfed9d677e7928e237f4d7a
opilane <opilane@HP-CZC6297BB4>
2022-12-19 14:11:25 +0200

    Add project files.

---
+++ if_opreator.py
@@ -0,0 +1,8 @@
+nimi = input("Mis sinu nimi on? ")
+if nimi = ("Juku") :
+    print("Läheme kinno!")
+    age = input("Kui vana sa oled? ")
+    if age<6 and age>0 :
+        print("Sinu pilet on tasuta")
+    elif age=>6 and age<14:
+        print("Sinu pilet on lastpilet")
